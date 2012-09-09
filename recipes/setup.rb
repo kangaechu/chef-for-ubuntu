32b12c88c3512c25ee682186097fb778a554d08c
Sytse Sijbrandij <sytse@dosire.com>
2012-09-09 21:33:05 +0200

    Add setup recipe and use public hostname. #1

---
+++ recipes/setup.rb
@@ -0,0 +1,25 @@
+execute "delete db" do
+  user "gitlab"
+  group "gitlab"
+  path "/home/gitlab/gitlab"
+  command "bundle exec rake db:drop RAILS_ENV=production"
+end
+
+execute "create db and load schema" do
+  user "gitlab"
+  group "gitlab"
+  path "/home/gitlab/gitlab"
+  command "bundle exec rake db:setup RAILS_ENV=production"
+end
+
+execute "seed db" do
+  user "gitlab"
+  group "gitlab"
+  path "/home/gitlab/gitlab"
+  command "bundle exec rake db:seed_fu RAILS_ENV=production"
+end
+
+execute "start service" do
+  command "sudo service gitlab start"
+end
+
