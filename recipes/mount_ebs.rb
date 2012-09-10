0c020c8ab090ec8308fe3dd0209e8925f22b54e4
Sytse Sijbrandij <sytse@dosire.com>
2012-09-10 22:08:20 +0200

    Attach new ebs volume automatically. #7

--- recipes/mount_ebs.rb
+++ recipes/mount_ebs.rb
@@ -1,6 +1,30 @@
-execute "remove if already mounted" do
-  command "umount /mnt"
+include_recipe "aws"
+aws = data_bag_item("services", "gitlab")["aws"]
+
+# https://github.com/opscode/cookbooks/tree/master/aws/#ebs_volumerb
+
+aws_ebs_volume "ebs_volume_new" do
+  aws_access_key aws['aws_access_key_id']
+  aws_secret_access_key aws['aws_secret_access_key']
+  size 50 # GB
+  device "/dev/sdi" # Shows up as something different.
+  description "repos-and-attachments"
+  action [ :create, :attach ]
 end
+
+# aws_ebs_volume "ebs_volume_from_snapshot" do
+#   aws_access_key aws['aws_access_key_id']
+#   aws_secret_access_key aws['aws_secret_access_key']
+#   size 50 # GB
+#   device "/dev/sdi"
+#   description "repos-and-attachments"
+#   snapshot_id "snap-ABCDEFGH"
+#   action [ :create, :attach ]
+# end
+
+# execute "remove if already mounted" do
+#   command "umount /mnt"
+# end
 
 # Mount point for EBS
 directory "/mnt/ebs" do
