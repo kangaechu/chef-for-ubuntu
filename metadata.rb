a47988b89cb165ecc5e36dbd7063474f66c32122
Sytse Sijbrandij <sytse@dosire.com>
2012-09-10 22:13:20 +0200

    AWS dependency. #7

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ version          "0.3.0"
 %w{ gitolite nginx }.each do |cb_conflict|
   conflicts cb_conflict
 end
-%w{ ruby_build git redisio build-essential python readline sudo openssh perl xml zlib}.each do |cb_depend|
+%w{ ruby_build git redisio build-essential python readline sudo openssh perl xml zlib aws}.each do |cb_depend|
   depends cb_depend
 end
 %w{ redhat centos scientific amazon debian ubuntu }.each do |os|
