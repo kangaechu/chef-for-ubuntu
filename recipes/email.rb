8fd3be5af9f918a03b02017b11ce0ede517220a0
Sytse Sijbrandij <sytse@dosire.com>
2012-09-13 22:16:21 +0200

    Set email config. #2

---
+++ recipes/email.rb
@@ -0,0 +1,17 @@
+cookbook_file "/home/gitlab/gitlab/config/initializers/smtp_enable_tls.rb" do
+  source "smtp_enable_tls.rb"
+  mode 0664
+  owner "gitlab"
+  group "gitlab"
+end
+
+template "/home/gitlab/gitlab/config/initializers/smtp_settings.rb" do
+  source "smtp_settings.rb.erb"
+  mode 0664
+  owner "gitlab"
+  group "gitlab"
+end
+
+execute "change rails production config from sendmail to smtp" do
+  command "sudo sed -i 's/:sendmail/:smtp/' /home/gitlab/gitlab/config/environments/production.rb"
+end
