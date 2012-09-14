347305b4efa93b101fd25bd6cb7fd8b680462b3e
Sytse Sijbrandij <sytse@dosire.com>
2012-09-14 15:58:52 +0200

    Change email message. #13

--- recipes/email.rb
+++ recipes/email.rb
@@ -18,3 +18,8 @@ end
 execute "change rails production config from sendmail to smtp" do
   command "sudo sed -i 's/:sendmail/:smtp/' /home/gitlab/gitlab/config/environments/production.rb"
 end
+
+execute "change email message" do
+  command "sudo sed -i 's/Administrator\ created\ account\ for\ you\.\ Now\ you\ are\ a\ member\ of\ company\ GitLab\ application\./An\ account\ was\ created\ for\ you\ on\ Gitlab\.com\./' /home/gitlab/gitlab/app/views/notify/new_user_email.html.haml"
+end
+
