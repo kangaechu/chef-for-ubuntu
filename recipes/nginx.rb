ee3fcf9dff108259f13f9cead1909bed9bd96447
Sytse Sijbrandij <sytse@dosire.com>
2012-09-14 11:06:37 +0200

    Use ssl. #4

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -3,13 +3,27 @@ package "nginx" do
   action :install
 end
 
+file "/etc/ssl/gitlab.com.key" do
+  content data_bag_item('services', 'gitlab')["certificate_key"]
+  owner "root"
+  group "root"
+  mode 0600
+end
+
+file "/etc/ssl/gitlab.com.crt" do
+  content data_bag_item('services', 'gitlab')["certificate_chained"]
+  owner "root"
+  group "root"
+  mode 0600
+end
+
 template "/etc/nginx/sites-available/gitlab" do
   source "gitlab.erb"
   mode 0664
   owner "root"
   group "root"
   variables(
-    :listen_port => 80
+    :ssl => data_bag_item('services', 'gitlab')['ssl'],
   )
 end
 
