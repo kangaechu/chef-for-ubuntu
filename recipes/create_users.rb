32e36617d945be4092c031cdf0f8b077ad63894b
Sytse Sijbrandij <sytse@dosire.com>
2012-09-24 17:52:12 +0200

    Make user adding idempotent. #26

--- recipes/create_users.rb
+++ recipes/create_users.rb
@@ -1,11 +1,14 @@
 execute "create git user" do
   command "adduser --system --shell /bin/sh --gecos 'git version control' --group --disabled-password --home /home/git git"
+  not_if "grep git /etc/passwd"
 end
 
 execute "create gitlab user" do
   command "adduser --disabled-login --gecos 'gitlab system' gitlab"
+  not_if "grep gitlab /etc/passwd"
 end
 
 execute "add gitlab to git group" do
   command "usermod -a -G git gitlab"
+  not_if "grep gitlab /etc/passwd"
 end
