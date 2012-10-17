1f2c2385d1f44cf9acb41ffcf76ed2c6878db31d
Marin Jankovski <maxlazio@gmail.com>
2012-10-17 16:15:29 +0200

    Start gitlab service.

--- recipes/restart.rb
+++ recipes/restart.rb
@@ -2,8 +2,8 @@ service "nginx" do
   action :restart
 end
 
-service "gitlab start" do
-  service_name "gitlab"
-  provider Chef::Provider::Service::Init::Debian
-  action :start
+execute "Gitlab start" do
+  user 'root'
+  group 'root'
+  command "service gitlab start"
 end
