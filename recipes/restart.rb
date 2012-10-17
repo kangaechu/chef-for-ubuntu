0b3a29f63c92804a91d4df90aa22e2f1f7191632
Marin Jankovski <maxlazio@gmail.com>
2012-10-17 09:09:25 +0200

    Try nohup.

--- recipes/restart.rb
+++ recipes/restart.rb
@@ -2,17 +2,17 @@ service "nginx" do
   action :restart
 end
 
-service "gitlab start" do
-  service_name "gitlab"
-  provider Chef::Provider::Service::Init::Debian
-  action :start
-end
-
-# execute "Gitlab start" do
-#   user 'root'
-#   group 'root'
-#   command "service gitlab start"
+# service "gitlab start" do
+#   service_name "gitlab"
+#   provider Chef::Provider::Service::Init::Debian
+#   action :start
 # end
+
+execute "Gitlab start" do
+  user 'root'
+  group 'root'
+  command "nohup bash -c 'service gitlab start'"
+end
 
 execute "Gitlab status" do
   user 'root'
