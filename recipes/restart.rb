4c77d6a3ae44cc2ceafa1aa1427cd469818c74b7
Marin Jankovski <maxlazio@gmail.com>
2012-10-17 08:37:11 +0200

    Use the regular chef service command for start.

--- recipes/restart.rb
+++ recipes/restart.rb
@@ -8,20 +8,14 @@ service "gitlab start" do
   action :start
 end
 
-service "gitlab status" do
-  service_name "gitlab"
-  provider Chef::Provider::Service::Init::Debian
-  action :status
-end
-
 # execute "Gitlab start" do
 #   user 'root'
 #   group 'root'
 #   command "service gitlab start"
 # end
 
-# execute "Gitlab status" do
-#   user 'root'
-#   group 'root'
-#   command "service gitlab status"
-# end
+execute "Gitlab status" do
+  user 'root'
+  group 'root'
+  command "service gitlab status"
+end
