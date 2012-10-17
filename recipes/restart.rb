5751f38c52729d8b0f46fdb99717647692598275
Marin Jankovski <maxlazio@gmail.com>
2012-10-17 15:25:22 +0200

    Remove extra comments from gitlab start.

--- recipes/restart.rb
+++ recipes/restart.rb
@@ -2,20 +2,8 @@ service "nginx" do
   action :restart
 end
 
-# service "gitlab start" do
-#   service_name "gitlab"
-#   provider Chef::Provider::Service::Init::Debian
-#   action :start
-# end
-
-execute "Gitlab start" do
-  user 'root'
-  group 'root'
-  command "/usr/bin/nohup sh -c 'service gitlab start >> /home/gitlab/gitlab/log/nohup.log' &>/dev/null </dev/null &"
-end
-
-execute "Gitlab status" do
-  user 'root'
-  group 'root'
-  command "service gitlab status >> /home/gitlab/gitlab/log/nohup.log"
+service "gitlab start" do
+  service_name "gitlab"
+  provider Chef::Provider::Service::Init::Debian
+  action :start
 end
