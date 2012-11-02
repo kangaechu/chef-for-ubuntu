c9d11f2b64da3788bff5301745cdb7b72db725f6
Marin Jankovski <maxlazio@gmail.com>
2012-11-02 10:44:17 +0100

    Run db setup only once.

--- recipes/setup_db.rb
+++ recipes/setup_db.rb
@@ -3,16 +3,7 @@ execute "delete db" do
   group "gitlab"
   cwd "/home/gitlab/gitlab"
   command "bundle exec rake db:drop RAILS_ENV=production"
-  notifies :create, "ruby_block[db_setup]", :immediately
   not_if { node.attribute?("db_setup") }
-end
-
-ruby_block "db_setup" do
-  block do
-    node.set['db_setup'] = true
-    node.save
-  end
-  action :nothing
 end
 
 execute "create db and load schema" do
@@ -28,7 +19,16 @@ execute "seed db" do
   group "gitlab"
   cwd "/home/gitlab/gitlab"
   command "bundle exec rake db:seed_fu RAILS_ENV=production"
+  notifies :create, "ruby_block[db_setup]", :immediately
   not_if { node.attribute?("db_setup") }
+end
+
+ruby_block "db_setup" do
+  block do
+    node.set['db_setup'] = true
+    node.save
+  end
+  action :nothing
 end
 
 execute "run status" do
