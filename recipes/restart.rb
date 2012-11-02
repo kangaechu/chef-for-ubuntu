ae86e28e580dc2b3b511edffa94d82c126f6375e
Marin Jankovski <maxlazio@gmail.com>
2012-11-02 09:37:45 +0100

    Use chef service to start gitlab.

--- recipes/restart.rb
+++ recipes/restart.rb
@@ -2,8 +2,6 @@ service "nginx" do
   action :restart
 end
 
-execute "Gitlab start" do
-  user 'root'
-  group 'root'
-  command "/etc/init.d/gitlab start"
+service "gitlab" do
+  action :start
 end
