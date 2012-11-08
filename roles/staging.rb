3287d7df7b48b4b183e720a52cfd12aca29f6b55
Marin Jankovski <maxlazio@gmail.com>
2012-11-08 08:47:43 +0100

    Create a staging role

---
+++ roles/staging.rb
@@ -0,0 +1,11 @@
+name "staging"
+description "Staging role for this cookbook"
+run_list(
+  "role[gitlab]",
+  "recipe[gitlab::setup_db]",
+  "recipe[gitlab::attach_eip]",
+  "recipe[gitlab::restart]"
+)
+default_attributes({
+  # All in databag
+})
