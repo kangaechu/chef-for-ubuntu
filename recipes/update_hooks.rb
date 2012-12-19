5f1c9e31f407f49d2a2f90085dea14239cb0d0cb
Marin Jankovski <maxlazio@gmail.com>
2012-12-19 14:34:27 +0100

    Add gitolite setup and db migrate to a recipe for production.

--- recipes/update_hooks.rb
+++ recipes/update_hooks.rb
@@ -1,3 +1,10 @@
+execute "gitolite setup" do
+  command "sudo -u git -H sh -c 'PATH=/home/git/bin:$PATH; gitolite setup -pk /home/git/gitlab.pub'"
+end
+
+execute "migrate the db" do
+  command "RAILS_ENV=production sudo -u gitlab bundle exec rake db:migrate"
+end
 execute "rewrite hooks in all projects to symlink gitolite hook" do
   command "sudo -u git -H /home/gitlab/gitlab/lib/support/rewrite-hooks.sh"
 end
