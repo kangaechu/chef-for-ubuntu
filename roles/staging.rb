20aa3af2e8104036af1aa67b438a32d079e22c40
Marin Jankovski <maxlazio@gmail.com>
2012-12-19 13:38:19 +0100

    Revert "Temporaily use staging role similar to production."
    This reverts commit 76c05767021dbaa16c6ebff49e666c4eb988485b.

--- roles/staging.rb
+++ roles/staging.rb
@@ -2,11 +2,11 @@ name "staging"
 description "Staging role for this cookbook"
 run_list(
   "role[gitlab]",
-  #"recipe[gitlab::setup_db]",
+  "recipe[gitlab::setup_db]",
   "recipe[gitlab::start]",
-  #"recipe[gitlab::attach_eip]",
-  #"recipe[gitlab::update_hooks]",
-  #"recipe[gitlab::restart]"
+  "recipe[gitlab::attach_eip]",
+  "recipe[gitlab::update_hooks]",
+  "recipe[gitlab::restart]"
 )
 default_attributes({
   # All in databag
