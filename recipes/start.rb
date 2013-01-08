ba96a40e8d3caff61e87d8033ba0a1691c8a193d
Marin Jankovski <maxlazio@gmail.com>
2013-01-08 09:53:43 +0100

    Dont use chef service magic.

--- recipes/start.rb
+++ recipes/start.rb
@@ -5,9 +5,7 @@ end
 begin
   print("Starting gitlab service")
   print(".")
-  status = `service gitlab status`
-  service "gitlab" do
-    action :start
-  end
+  status = `sudo service gitlab status`
+  `sudo service gitlab start`
   sleep(5)
 end while "Gitlab service is not running." == status
