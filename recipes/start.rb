6a1b95b4bd6e1b8bba76a6d9ebacd706e439044d
Marin Jankovski <maxlazio@gmail.com>
2013-01-09 09:51:56 +0100

    Increase sleep to catch the output.

--- recipes/start.rb
+++ recipes/start.rb
@@ -2,10 +2,10 @@ service "nginx" do
   action :start
 end
 
-puts("Starting gitlab service")
+print("Starting gitlab service")
 `sudo service gitlab start`
 begin
   print(".")
-  sleep(1)
+  sleep(5)
   status = `sudo service gitlab status`
 end while "Gitlab service is not running." == status
