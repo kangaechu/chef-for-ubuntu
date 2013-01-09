6871756a9c4d9c6020bc6744167da63c618fca0f
Marin Jankovski <maxlazio@gmail.com>
2013-01-09 09:45:58 +0100

    Expand.

--- recipes/start.rb
+++ recipes/start.rb
@@ -2,6 +2,10 @@ service "nginx" do
   action :start
 end
 
-print("Starting gitlab service")
+puts("Starting gitlab service")
 `sudo service gitlab start`
-print(".") && sleep(1) while "Gitlab service is not running." == `sudo service gitlab status`
+begin
+  print(".")
+  sleep(1)
+  status = `sudo service gitlab status`
+end while "Gitlab service is not running." == status
