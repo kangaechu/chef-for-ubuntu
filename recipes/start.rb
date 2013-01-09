de0cfdbd3b831807218105a3b4fb3ff67bee5324
Marin Jankovski <maxlazio@gmail.com>
2013-01-09 12:24:14 +0100

    Use system call for service start.

--- recipes/start.rb
+++ recipes/start.rb
@@ -3,9 +3,10 @@ service "nginx" do
 end
 
 print("Starting gitlab service")
-`sudo service gitlab start`
+system('sudo service gitlab start')
 begin
   print(".")
   sleep(5)
   status = `sudo service gitlab status`
 end while "Gitlab service is not running." == status
+puts(" done.")
