9d4f27c5bbfab1c5dcbfff342007fde47145e4c5
Marin Jankovski <maxlazio@gmail.com>
2013-01-09 13:46:48 +0100

    Log the status

---
+++ recipes/start.rb
@@ -0,0 +1,21 @@
+service "nginx" do
+  action :start
+end
+
+print("Starting gitlab service")
+# bash "start gitlab" do
+#   code <<-EOH
+#     sudo service gitlab start
+#   EOH
+# end
+service "gitlab" do
+  action :start
+end
+
+begin
+  print(".")
+  status = `sudo service gitlab status`
+  sleep(5)
+  Chef::Log.info("++++++++ #{status}")
+end while "Gitlab service is not running." == status
+puts(" done.")
